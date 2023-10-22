a61d5f1e01378e70f756bb76644ac2034d8c45d2
Faiq Bokhari <bokhars@mcmaster.ca>
2023-10-22 18:08:21 -0400

    sidebar ui update

--- vid_share/src/components/Menu.jsx
+++ vid_share/src/components/Menu.jsx
@@ -1,68 +1,84 @@
-import React from 'react'
-import styled from "styled-components";
+import React from 'react';
+import styled from 'styled-components';
 import tutor_icon from "../img/tutor_icon.PNG";
 import HomeIcon from '@mui/icons-material/Home';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
-
+import ExploreIcon from '@mui/icons-material/Explore';
+import SubscriptionsIcon from '@mui/icons-material/Subscriptions';
+import LibraryBooksIcon from '@mui/icons-material/LibraryBooks';
+import HistoryIcon from '@mui/icons-material/History';
+import SettingsIcon from '@mui/icons-material/Settings';
+import HelpIcon from '@mui/icons-material/Help';
+import Brightness4Icon from '@mui/icons-material/Brightness4';
 
 const Container = styled.div`
-    flex: 1;
-    background-color:  #2153be;
-    height: 100vh;
-    color: white;   
-    position: sticky;
-    top: 0%;
+  width: 280px;
+  background-color: #0A1C3E;
+  height: 100vh;
+  color: white;
+  position: sticky;
+  top: 0;
+  font-family: 'Roboto', sans-serif;
 `;
 
 const Wrapper = styled.div`
-    padding: 18px 26px;
+  padding: 24px;
 `;
 
 const Logo = styled.div`
   display: flex;
   align-items: center;
-  gap: 20px;
   font-weight: bold;
-  margin-bottom: 25px;
-  font-size: 40px;
+  margin-bottom: 32px;
+  font-size: 28px;
 `;
 
 const Img = styled.img`
   height: 50px;
-
-`
+`;
 
 const Item = styled.div`
   display: flex;
   align-items: center;
-  gap: 20px;
   cursor: pointer;
-  font-size: 30px;
-  padding: 7.5px 0px;
+  font-size: 20px;
+  padding: 12px 0;
+  gap: 10px;
+  transition: background-color 0.2s;
+
+  &:hover {
+    background-color: #008080;
+  }
 `;
 
 const Hr = styled.hr`
-  margin: 15px 0px;
+  margin: 24px 0;
   border: 0.5px solid #4e74c5;
 `;
 
 const Login = styled.div`
-  font-size: 30px;
+  font-size: 20px;
+  margin-top: 24px;
 `;
 
 const Button = styled.button`
-  font-size: 30px;
-  padding: 5px 15px;
-  background-color: transparent;
-  border: 1px solid white;
+  font-size: 20px;
+  padding: 8px 16px;
+  background-color: #2153be;
+  border: none;
   border-radius: 10px;
   font-weight: 500;
-  color: azure;
+  color: white;
   margin-top: 10px;
   cursor: pointer;
   display: flex;
   align-items: center;
-  gap: 20px;
+  gap: 10px;
+  transition: background-color 0.2s;
+
+  &:hover {
+    background-color: #3b69c9;
+  }
 `;
 
 const Menu = () => {
@@ -70,54 +86,54 @@ const Menu = () => {
     <Container>
       <Wrapper>
         <Logo>
-          <Img src={tutor_icon}/>
-            TutorVision
+          <Img src={tutor_icon} alt="TutorVision" />
+          TutorVision
         </Logo>
         <Item>
-          <HomeIcon fontSize='large'></HomeIcon>
+          <HomeIcon fontSize="large" />
           Home
         </Item>
         <Item>
-          <HomeIcon fontSize='large'></HomeIcon>
+          <ExploreIcon fontSize="large" />
           Explore
         </Item>
         <Item>
-          <HomeIcon fontSize='large'></HomeIcon>
+          <SubscriptionsIcon fontSize="large" />
           Subscriptions
         </Item>
-        <Hr/>
+        <Hr />
         <Item>
-          <HomeIcon fontSize='large'></HomeIcon>
+          <LibraryBooksIcon fontSize="large" />
           Library
         </Item>
         <Item>
-          <HomeIcon fontSize='large'></HomeIcon>
+          <HistoryIcon fontSize="large" />
           History
         </Item>
-        <Hr/>
+        <Hr />
         <Login>
           Sign in to subscribe and comment
           <Button>
-            <AccountCircleIcon fontSize='large'></AccountCircleIcon>
+            <AccountCircleIcon fontSize="large" />
             Sign in
           </Button>
         </Login>
-        <Hr/>
+        <Hr />
         <Item>
-          <HomeIcon fontSize='large'></HomeIcon>
+          <SettingsIcon fontSize="large" />
           Settings
         </Item>
         <Item>
-          <HomeIcon fontSize='large'></HomeIcon>
+          <HelpIcon fontSize="large" />
           Help
         </Item>
         <Item>
-          <HomeIcon fontSize='large'></HomeIcon>
+          <Brightness4Icon fontSize="large" />
           Light Mode
         </Item>
       </Wrapper>
     </Container>
-  )
-}
+  );
+};
 
-export default Menu
+export default Menu;
